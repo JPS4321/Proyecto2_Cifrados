424518122d8de629ebd3f0e94f930576d7e84932
AndyOK <andyortegakou@yahoo.com.mx>
2026-05-04 21:57:23 -0600

    feat(api): register messages router in main application

--- src/main.py
+++ src/main.py
@@ -5,11 +5,13 @@ from sqlalchemy.orm import Session
 from src.database import get_db
 from src.routes.auth import router as auth_router
 from src.routes.users import router as users_router
+from src.routes.messages import router as messages_router
 
 app = FastAPI(title="Proyecto2 API")
 
 app.include_router(auth_router)
 app.include_router(users_router)
+app.include_router(messages_router)
 
 @app.get("/")
 def root():
